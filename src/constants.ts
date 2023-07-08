fbcc9373f42f9ac23227c139b9a5c8e62c4815fc
Jose Cerqueira <josecerqueira@protonmail.com>
2023-07-08 01:30:53 +0100

    feat: ban aipx pool

--- src/constants.ts
+++ src/constants.ts
@@ -2,10 +2,6 @@ export const DEX_PACKAGE_ID =
   '0x5c45d10c26c5fb53bfaff819666da6bc7053d2190dfa29fec311cc666ff1f4b0';
 
 export const POOLS_TVL_BLACK_LIST = [
-  // SUI/AIFrens
-  '0x747bd9685c9d9325fe536bdde891a2fdf7ef742ceb3aaacd972a3f0ded1050c2',
-  // SUI/ORDI
-  '0x75f4791ab1e6e2e48d29361abf1fc56ae3f9d7c4f0680d4e5bafa3c1ba8a9bc6',
-  //ETHSUISHIB
-  '0x451d3cca15b03b7d28a9d0a0d48ff22e9ba53c6e7b2b5123fdc77cfe4e4bd6a9',
+  // aIPX pool
+  '0x9ce38812c9afb81dc31aacbd12d7783d734428c8dae20e107fc47c69c1b18d1f',
 ];
